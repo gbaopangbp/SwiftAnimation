92f6e454ca993b2a897a6cce086e1f296047311b
yaoyingtao <yaoyingtao@yaoyingtaodeMBP.lan>
2016-02-21 17:09:18 +0800

    完成Chapter2

--- iOS Animations by Tutorials v2.0/Chapter 01 - Getting Started with View Animations/BahamaAir-Starter/BahamaAirLoginScreen/ViewController.swift
+++ iOS Animations by Tutorials v2.0/Chapter 01 - Getting Started with View Animations/BahamaAir-Starter/BahamaAirLoginScreen/ViewController.swift
@@ -79,11 +79,58 @@ class ViewController: UIViewController, UITextFieldDelegate {
   
   override func viewWillAppear(animated: Bool) {
     super.viewWillAppear(animated)
+    heading.center.x -= view.bounds.size.width
+    username.center.x -= view.bounds.size.width
+    password.center.x -= view.bounds.size.width
     
+    cloud1.alpha = 0.0
+    cloud2.alpha = 0.0
+    cloud3.alpha = 0.0
+    cloud4.alpha = 0.0
+    
+    loginButton.frame.origin.y += 30
+    loginButton.alpha = 0.0
+
   }
   
   override func viewDidAppear(animated: Bool) {
     super.viewDidAppear(animated)
+    
+    UIView.animateWithDuration(0.5, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
+        self.heading.center.x += self.view.bounds.width
+
+        }, completion: nil)
+    
+    UIView.animateWithDuration(0.5, delay: 0.3, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
+        self.username.center.x += self.view.bounds.width
+        
+        }, completion: nil)
+    
+    UIView.animateWithDuration(0.5, delay: 0.4, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
+        self.password.center.x += self.view.bounds.width
+        
+        }, completion: nil)
+    
+    UIView.animateWithDuration(0.5, delay: 0.5, options: [], animations: {
+        self.cloud1.alpha = 1.0
+        }, completion: nil)
+    
+    UIView.animateWithDuration(0.5, delay: 0.7, options: [], animations: {
+        self.cloud2.alpha = 1.0
+        }, completion: nil)
+    
+    UIView.animateWithDuration(0.5, delay: 0.9, options: [], animations: {
+        self.cloud3.alpha = 1.0
+        }, completion: nil)
+    
+    UIView.animateWithDuration(0.5, delay: 1.1, options: [], animations: {
+        self.cloud4.alpha = 1.0
+        }, completion: nil)
+    
+    UIView.animateWithDuration(0.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
+        self.loginButton.frame.origin.y -= 30
+        self.loginButton.alpha = 1.0
+        }, completion: nil)
     
   }
   
@@ -91,6 +138,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
   
   @IBAction func login() {
     view.endEditing(true)
+    UIView.animateWithDuration(1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
+        self.loginButton.bounds.size.width += 80
+        }, completion: nil)
+    
+    UIView.animateWithDuration(0.33, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: [], animations: {
+        self.loginButton.frame.origin.y += 60
+        self.loginButton.backgroundColor = UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0)
+        self.spinner.center = CGPoint(x: 40.0, y: self.loginButton.frame.size.height/2)
+        self.spinner.alpha = 1.0
+        }, completion: nil)
     
   }
   
